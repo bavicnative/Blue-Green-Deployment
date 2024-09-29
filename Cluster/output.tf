6915c3d693f998bd76f2325ab03d62ad3e7ce463
Bavithran M <79276699+bavicnative@users.noreply.github.com>
2024-09-29 21:33:23 +0530

    Update output.tf
    careerbytecode

--- Cluster/output.tf
+++ Cluster/output.tf
@@ -1,16 +1,16 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.careerbytecode.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.careerbytecode.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.careerbytecode_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.careerbytecode_subnet[*].id
 }
 
